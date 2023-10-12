b3f91107f43f32f4dc630f0ca533168230acf72b
Yusra Abdulrahman <yusraabdulrahman@Yusras-MacBook-Pro.local>
2023-10-12 11:29:07 +0200

    initial commit

---
+++ challenges/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+## URL Config for challenges app
+urlpatterns = [
+    #path("january", views.january),
+    #path("february", views.february),
+    #path("march", views.march),
+
+    path("", views.index, name="index"),
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge, name = "month-challenge")
+]
+
